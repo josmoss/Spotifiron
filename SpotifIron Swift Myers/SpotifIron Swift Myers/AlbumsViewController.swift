19b5203aba765e8f417c5b31300a0f187b3ff629
josmoss <josmoss@gmail.com>
2016-06-17 15:44:37 -0600

    Created AlbumnViewController and set up View Controller file.

---
+++ SpotifIron Swift Myers/SpotifIron Swift Myers/AlbumsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AlbumsViewController.swift
+//  SpotifIron Swift Myers
+//
+//  Created by Joe Moss on 6/17/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK : Table View Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView .dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        return cell
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
