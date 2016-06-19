5100f9876df6f1a48973f26dddd7021381d771fc
Chris Myers <cmyers78@gmail.com>
2016-06-19 14:53:35 -0600

    currently working on custom tableview cells....in flux

---
+++ SpotifIron Swift Myers/SpotifIron Swift Myers/ArtistTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistTableViewCell.swift
+//  SpotifIron Swift Myers
+//
+//  Created by Christopher Myers on 6/19/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var artistImageView: UIImageView!
+
+    @IBOutlet weak var artistNameLabel: UILabel!
+
+    @IBOutlet weak var popularityScoreLabel: UILabel!
+}
