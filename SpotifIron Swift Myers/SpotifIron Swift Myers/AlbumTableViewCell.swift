7fa360880c71c9972f06703fe787680c2c6c6d45
josmoss <josmoss@gmail.com>
2016-06-19 18:35:45 -0600

    Created AlbumTableViewCell.

---
+++ SpotifIron Swift Myers/SpotifIron Swift Myers/AlbumTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumTableViewCell.swift
+//  SpotifIron Swift Myers
+//
+//  Created by Joe Moss on 6/19/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var albumNameLabel: UILabel!
+    
+}
